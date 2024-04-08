b74290640830f3418f5a162aad4190931e48ad85
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2024-04-08 19:46:33 +0200

    maksimum

---
+++ src/_01_instrukcje/Maksimum.java
@@ -0,0 +1,15 @@
+package _01_instrukcje;
+
+public class Maksimum {
+	public static void main(String[] args) {
+		int a = 1, b = 3;
+		int w = 0;
+
+		if (a > b) {
+			w = a;
+		} else {
+			w = b;
+		}
+		System.out.println(w);
+	}
+}
