9bd86cb0c71d810180bc94f2c9d1d0af0042457b
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2024-04-22 20:25:29 +0200

    Whiteboard.pdf + Imiona.java

--- src/Lab/Imiona.java
+++ src/Lab/Imiona.java
@@ -1,7 +1,19 @@
 package Lab;
 
+
 public class Imiona {
 	public static void main(String[] args) {
+		int N = 10;
+		String[] imiona = new String[N];
 
+		for(int i = 0; i < N; i++)
+			imiona[i] = i + "Ala" + (i+1);
+
+		int maks = 0;
+		for(int i = 1; i < N; i++)
+			if (imiona[i].length() > imiona[maks].length()){
+				maks = i;
+			}
+		System.out.println(imiona[maks]);
 	}
 }
